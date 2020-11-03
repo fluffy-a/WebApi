9f0411a2292ba52a092ac2a7ab19ad1ce55a7801
fluffy-a <atwefashionit@gmail.com>
2020-11-03 23:53:25 +0100

    //

---
+++ 1.Project/LoginAuth/LoginAuth/Areas/Identity/Data/LoginAuthContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginAuth.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Azure.Documents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Office.Interop.Outlook;
+using Microsoft.VisualStudio.Services.Account;
+using Account = Microsoft.VisualStudio.Services.Account.Account;
+
+namespace LoginAuth.Data
+{
+    public class LoginAuthContext : IdentityDbContext<LoginAuthUser>
+    {
+        public LoginAuthContext(DbContextOptions<LoginAuthContext> options)
+            : base(options)
+        {
+        }
+       
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+        DbSet<User> Users { get; set; }
+        DbSet<AccountClass> AccountClasses { get; set; }
+        DbSet<Account> Accounts { get; set; }
+        DbSet<AccountType> AccountTypes { get; set; }
+        DbSet<Blagajna> Blagajna { get; set; }
+        DbSet<Clients> Clients { get; set; }
+    }
+}
---
+++ 1.Project/LoginAuth/LoginAuth/Areas/Identity/Data/LoginAuthUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace LoginAuth.Areas.Identity.Data
+{
+    //database
+    // Add profile data for application users by adding properties to the LoginAuthUser class
+    public class LoginAuthUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
